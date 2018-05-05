405a696342eced48be5e7ec878563b8977f4203f
Adam Rackis <adamrackis@hotmail.com>
2018-05-05 18:50:08 -0500

    styling tweak

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Home.css";
+import Convert from "ansi-to-html";
+const convert = new Convert();
 
 const spawn = require("cross-spawn");
 const cliStyles = {
@@ -54,7 +56,62 @@ class Webpack extends Component {
   render() {
     let { style, ...rest } = this.props;
     return (
-      <div style={{ ...style }} {...rest}>
+      <div style={{ ...style, overflow: "hidden" }} {...rest}>
+        <div style={{}}>
+          <button className={styles.btn}>
+            <i className="fas fa-ban" />
+          </button>
+          <button className={styles.btn}>
+            <i className="far fa-sync" />
+          </button>
+        </div>
+        <pre dangerouslySetInnerHTML={{ __html: this.state.output }} ref={el => (this.outputEl = el)} style={cliStyles} />
+      </div>
+    );
+  }
+}
+
+class TS extends Component {
+  state = { output: "" };
+  componentDidMount() {
+    let wp = spawn("npm.cmd", ["run", "tscw"], { cwd: "c:/git/MainLine/members" });
+    this.wp = wp;
+    wp.on("error", (a, b, c) => {
+      debugger;
+    });
+    debugger;
+    wp.stdout.on("data", (text, b, c) => {
+      debugger;
+      let X = text.toString("utf8");
+      let Y = convert.toHtml(text.toString("utf8"));
+
+      debugger;
+      text = convert.toHtml(text.toString("utf8"));
+
+      if (this.unmounted) {
+        this.cleanup();
+        return;
+      }
+      let old = this.state.output;
+      let output = old + text;
+      this.setState({ output }, () => {
+        this.outputEl.scrollTop = this.outputEl.scrollHeight;
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.cleanup();
+  }
+  cleanup() {
+    this.unmounted = true;
+    try {
+      this.wp.kill();
+    } catch (er) {}
+  }
+  render() {
+    let { style, ...rest } = this.props;
+    return (
+      <div style={{ ...style, overflow: "hidden" }} {...rest}>
         <div style={{}}>
           <button className={styles.btn}>
             <i className="fas fa-ban" />
@@ -74,20 +131,10 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className={styles.container} style={{ display: "flex" }}>
+        <div className={styles.container} style={{ display: "flex", overflow: "hidden" }}>
           <Webpack style={{ display: "flex", flexDirection: "column", padding: 5, flex: 2 }} />
           <div style={{ display: "flex", flexDirection: "column", padding: 5, flex: 1 }}>
-            <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-              <div style={{}}>
-                <button className={styles.btn}>
-                  <i className="fas fa-ban" />
-                </button>
-                <button className={styles.btn}>
-                  <i className="far fa-sync" />
-                </button>
-              </div>
-              <pre dangerouslySetInnerHTML={{ __html: this.state.output }} ref={el => (this.outputEl = el)} style={cliStyles} />
-            </div>
+            <TS style={{ flex: 1, display: "flex", flexDirection: "column" }} />
             <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
               <div style={{}}>
                 <button className={styles.btn}>
